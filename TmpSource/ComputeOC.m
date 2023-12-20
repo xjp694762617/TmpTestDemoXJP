b6abe621a41c9fae6d1ad73c4b7c3ef2c1a1cf50
jianpeng.xu <jianpeng.xu@dhc.com.cn>
2023-12-20 10:45:23 +0800

    第一次提交

---
+++ TmpSource/ComputeOC.m
@@ -0,0 +1,14 @@
+//
+//  ComputeOC.m
+//  ffdfffff
+//
+//  Created by jianpeng.xu on 2023/12/20.
+//
+
+#import "ComputeOC.h"
+
+@implementation ComputeOC
+- (int)parm:(int)start withB:(int)end {
+    return (start + end);
+}
+@end
